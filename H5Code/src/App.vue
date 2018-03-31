8555afa9bf7c766fa6f16a87a5b716f4698fc794
王来彬 <yourswanglaibin@126.com>
2018-03-31 19:04:36 +0800

    首页

--- H5Code/src/App.vue
+++ H5Code/src/App.vue
@@ -1,19 +1,102 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+ <div id="app" style="height:100%;">
+    <view-box ref="viewBox">
+      <x-header slot="header" :left-options="{showBack: false}" style="width:100%;position:absolute;left:0;top:0;z-index:100;">
+        <span class="color_1">创</span><span class="color_2">闻</span><span class="color_3">阅</span><span class="color_4">读</span>
+        <x-icon slot="right" type="navicon" size="35" style="fill:#F83E91;position:relative;top:-8px;left:-3px;"></x-icon>
+      </x-header>
+      <transition>
+        <router-view class="router-view main"></router-view>
+      </transition>
+      <tabbar slot="bottom">
+        <tabbar-item selected>
+          <img slot="icon" src="./assets/tabbar/icon_home.png">
+          <img slot="icon-active" src="./assets/tabbar/icon_home_red.png">
+          <span slot="label">主页</span>
+        </tabbar-item>
+        <tabbar-item>
+          <img slot="icon" src="./assets/tabbar/icon_books.png">
+          <img slot="icon-active" src="./assets/tabbar/icon_books_red.png">
+          <span slot="label">阅读记录</span>
+        </tabbar-item>
+        <tabbar-item link="/component/demo">
+          <img slot="icon" src="./assets/tabbar/icon_setting.png">
+          <img slot="icon-active" src="./assets/tabbar/icon_setting_red.png">
+          <span slot="label">设置</span>
+        </tabbar-item>
+      </tabbar>
+    </view-box>
+ </div>
 </template>
 
 <script>
+import { XHeader, TransferDom, Tabbar, TabbarItem, ViewBox } from 'vux'
 export default {
-  name: 'app'
+  directives: {
+    TransferDom
+  },
+  name: 'app',
+  components: {
+    XHeader,
+    Tabbar,
+    TabbarItem,
+    ViewBox
+  }
 }
 </script>
 
 <style lang="less">
-@import '~vux/src/styles/reset.less';
-
-body {
-  background-color: #fbf9fe;
-}
+@import "~vux/src/styles/reset.less";
+@import '~vux/src/styles/1px.less';
+@import '~vux/src/styles/tap.less';
+  html, body {
+    height: 100%;
+    width: 100%;
+    overflow-x: hidden;
+    background-color: #E8E8E8;
+  }
+  #app .vux-header,
+  #app .weui-tabbar {
+    background-color: #FFFFFF;
+  }
+  .vux-header .vux-header-title > span.color1{}
+  #app .vux-header:before{
+    content: " ";
+    position: absolute;
+    left: 0;
+    bottom: -1px;
+    right: 0;
+    height: 1px;
+    border-bottom: 1px solid #DDDDDD;
+    color: #DDDDDD;
+    -webkit-transform-origin: 0 0;
+    transform-origin: 0 0;
+    -webkit-transform: scaleY(0.5);
+    transform: scaleY(0.5);
+  }
+  #app .weui-tabbar:before{
+    border-top: 1px solid #DDDDDD;
+    color: #DDDDDD;
+  }
+  #app .weui-tabbar__item.weui-bar__item_on .weui-tabbar__label{
+    color: #F83E91;
+  }
+  .vux-header .vux-header-title > span{
+    padding: 0 8px;
+  }
+  .vux-header .vux-header-title > span.color_1{
+    color: #FFC320;
+  }
+  .vux-header .vux-header-title > span.color_2{
+    color: #62BFFC;
+  }
+  .vux-header .vux-header-title > span.color_3{
+    color: #9E5AFF;
+  }
+  .vux-header .vux-header-title > span.color_4{
+    color: #F83E91;
+  }
+  .main{
+    margin: 47px 0 10px;
+  }
 </style>
---
+++ H5Code/src/components/home.vue
@@ -0,0 +1,253 @@
+<template>
+  <div class="home">
+    <swiper :list="listData" auto height="180px" dots-position="center" ></swiper>
+    <divider class="spance"></divider>
+    <div class="grid">
+      <grid :show-lr-borders="false" :show-vertical-dividers="false">
+        <grid-item link="/component/cell">
+          <img slot="icon" src="../assets/home/icon_grid_1.png">
+          <span slot="label">开始记录</span>
+        </grid-item>
+        <grid-item :link="{ path: '/component/cell'}">
+          <img slot="icon" src="../assets/home/icon_grid_2.png">
+          <span slot="label">好书严选</span>
+        </grid-item>
+        <grid-item link="/component/cell">
+          <img slot="icon" src="../assets/home/icon_grid_3.png">
+          <span slot="label">书籍测试</span>
+        </grid-item>
+      </grid>
+      <grid :show-lr-borders="false" :show-vertical-dividers="false">
+        <grid-item link="/component/cell">
+          <img slot="icon" src="../assets/home/icon_grid_4.png">
+          <span slot="label">绑定学生</span>
+        </grid-item>
+        <grid-item :link="{ path: '/component/cell'}">
+          <img slot="icon" src="../assets/home/icon_grid_5.png">
+          <span slot="label">商务合作</span>
+        </grid-item>
+        <grid-item link="/component/cell">
+          <img slot="icon" src="../assets/home/icon_grid_6.png">
+          <span slot="label">关于我们</span>
+        </grid-item>
+      </grid>
+    </div>
+    <flexbox orient="vertical" :gutter="0">
+      <flexbox-item>
+        <div class="middel_banner">
+          <img style="display:block;" height="100%" width="100%" src="../assets/home/middel_banner.png" />
+        </div>
+      </flexbox-item>
+      <flexbox-item>
+        <div class="rb-panel">
+          <div class="rb-panel__bd">
+            <div v-for='(item,index) in list' class="rb-media-box">
+              <div class="rb-media-box_appmsg">
+                <div class="rb-media-box__hd">
+                  <img :src="item.src" alt="" class="rb-media-box__thumb">
+                </div>
+                <div class="rb-media-box__bd">
+                  <div class="rb-media-box__title">{{ item.title }}</div>
+                  <div class="rb-media-box__time">{{ item.date }}</div>
+                  <div class="rb-media-box__desc">
+                    <span>阅读量：{{ item.readCount }}</span><span class="icon-xin">{{ item.xinCount }}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </flexbox-item>
+    </flexbox>
+  </div>
+</template>
+
+<script>
+import { Panel, Group, Cell, Flexbox, FlexboxItem, Grid, GridItem, Swiper, SwiperItem, Divider } from 'vux'
+
+export default {
+  components: {
+    Group,
+    Cell,
+    Panel,
+    Divider,
+    Flexbox,
+    FlexboxItem,
+    Grid,
+    GridItem,
+    Swiper,
+    SwiperItem
+  },
+  methods: {
+    onItemClick () {
+      console.log('on item click')
+    },
+    onImgError (item, $event) {
+      console.log(item, $event)
+    }
+  },
+  data () {
+    return {
+      listData: [{
+        url: 'javascript:',
+        img: 'https://static.vux.li/demo/1.jpg'
+      }, {
+        url: 'javascript:',
+        img: 'https://static.vux.li/demo/2.jpg'
+      }, {
+        url: 'javascript:',
+        img: 'https://static.vux.li/demo/1.jpg'
+      }],
+      list: [{
+        src: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+        title: '标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一',
+        readCount: 20,
+        xinCount: 200,
+        url: '/component/cell',
+        date: '2017-12-12'
+      }, {
+        src: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+        title: '标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一',
+        readCount: 20,
+        xinCount: 200,
+        url: '/component/cell',
+        date: '2017-12-12'
+      }, {
+        src: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+        title: '标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一',
+        readCount: 20,
+        xinCount: 200,
+        url: '/component/cell',
+        date: '2017-12-12'
+      }, {
+        src: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+        title: '标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一',
+        readCount: 20,
+        xinCount: 200,
+        url: '/component/cell',
+        date: '2017-12-12'
+      }, {
+        src: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+        title: '标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一标题一',
+        readCount: 20,
+        xinCount: 200,
+        url: '/component/cell',
+        date: '2017-12-12'
+      }]
+    }
+  }
+}
+</script>
+
+<style>
+  .spance{
+    height: 10px;
+  }
+  .home .vux-slider > .vux-indicator > a > .vux-icon-dot{
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    background-color: #FFFFFF;
+  }
+  .home .vux-slider > .vux-indicator > a > .vux-icon-dot.active{
+    background-color: #EA2C06;
+  }
+  .home .weui-grid__icon{
+    width: 80px;
+    height: 80px;
+  }
+  .grid{
+    background-color: #FFFFFF;
+    margin-top: 10px;
+  }
+  .grid .weui-grids:before,
+  .grid .weui-grid:after{
+    height: 0;
+    border: 0;
+  }
+  .rb-panel{
+    background-color: #FFFFFF;
+    margin-top: 10px;
+    position: relative;
+    overflow: hidden;
+  }
+  .rb-panel:first-child{
+    margin-top: 0;
+  }
+  .rb-panel:before {
+    content: " ";
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    height: 1px;
+    border-top: 1px solid #E5E5E5;
+    color: #E5E5E5;
+    -webkit-transform-origin: 0 0;
+    transform-origin: 0 0;
+    -webkit-transform: scaleY(0.5);
+    transform: scaleY(0.5);
+  }
+  .rb-media-box {
+    padding: 10px;
+    position: relative;
+    margin-top: 10px;
+  }
+  .rb-media-box_appmsg {  
+    position: relative;
+  }
+  .rb-media-box_appmsg .rb-media-box__hd {
+    width: 60px;
+    height: 60px;
+    line-height: 60px;
+    text-align: center;
+    display: inline-block;
+  }
+  .rb-media-box_appmsg .rb-media-box__bd {
+    position: absolute;
+    top: 0;
+    left: 65px;
+  }
+  .rb-media-box_appmsg .rb-media-box__thumb {
+    width: 100%;
+    max-height: 100%;
+    vertical-align: top;
+    border-radius: 8px;
+  }
+  .rb-media-box__title{
+    font-size: 15px;
+    line-height: 1.3;
+    word-wrap: break-word;
+    word-break: break-all;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    padding-right: 75px;
+  }
+  .rb-media-box__time{
+    color: #999999;
+    font-size: 12px;
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+  .rb-media-box__desc {
+    color: #999999;
+    font-size: 13px;
+    margin-top: 5px;
+  }
+  .icon-xin{
+    margin-left: 20px;
+  }
+  .icon-xin::before{
+    content: '';
+    background: url(../assets/home/icon_xin.png) center center no-repeat;
+    width: 17px;
+    height: 17px;
+    display: inline-block;
+    background-size: cover;
+    vertical-align: sub;
+  }
+</style>
